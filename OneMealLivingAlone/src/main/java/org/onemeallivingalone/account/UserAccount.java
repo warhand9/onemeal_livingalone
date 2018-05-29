404d073c7c27fbba118c8d576aa84c8911088353
Ahasil <lazyahasil+dev@gmail.com>
2018-05-29 21:32:58 +0900

    Implemented "UserAccount"'s favorite list methods

--- OneMealLivingAlone/src/main/java/org/onemeallivingalone/account/UserAccount.java
+++ OneMealLivingAlone/src/main/java/org/onemeallivingalone/account/UserAccount.java
@@ -16,6 +16,21 @@ public class UserAccount extends Account {
 	private static final String EMAIL_REGEX = "^[_a-zA-Z0-9\\.+-]+@[_a-zA-Z0-9\\.+-]+\\.[a-zA-Z]+$"; /// < 이메일 정규 표현식
 	private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX); /// < 이메일 Pattern 객체
 
+	/**
+	 * 이메일 주소의 유효성을 검사합니다.
+	 * 
+	 * 이메일 주소는 UserAccount.EMAIL_REGEX 에 지정된 정규 표현식에 맞아야 합니다.
+	 * 
+	 * @param email
+	 *            이메일 주소
+	 * @return 이메일 주소의 유효성 여부
+	 */
+	public static boolean checkEmailValidity(String email) {
+		if (email == null)
+			return false;
+		return EMAIL_PATTERN.matcher(email).matches();
+	}
+
 	private String email; /// < 이메일 주소
 	private List<Integer> personalIngredients = new ArrayList<Integer>(); /// < 개인 식재료 목록
 	private List<Integer> personalFavoriteFoods = new ArrayList<Integer>(); /// < 관심 요리 목록
@@ -28,32 +43,16 @@ public class UserAccount extends Account {
 		this.email = email;
 	}
 
-	public void printPersonalIngredients() {
+	public void addFoodReview(Integer reviewId) {
+		personalFoodReviews.add(0, reviewId);
 	}
 
-	public void printPersonalFavoriteFoods() {
-	}
-
-	public void printpersonalFoodReviews() {
+	public void addPeronsalFavoriteFood(Integer foodId) {
+		personalFavoriteFoods.add(0, foodId);
 	}
 
 	public void addPersonalIngredient(Integer ingredientId) {
 		personalIngredients.add(0, ingredientId);
-	}
-
-	public void deletePersonalIngredient() {
-	}
-
-	public void addPeronsalFood() {
-	}
-
-	public void deletePersonalFood() {
-	}
-
-	public void addFoodReview() {
-	}
-
-	public void deleteFoodReview() {
 	}
 
 	/**
@@ -73,19 +72,16 @@ public class UserAccount extends Account {
 		return true;
 	}
 
-	/**
-	 * 이메일 주소의 유효성을 검사합니다.
-	 * 
-	 * 이메일 주소는 UserAccount.EMAIL_REGEX 에 지정된 정규 표현식에 맞아야 합니다.
-	 * 
-	 * @param email
-	 *            이메일 주소
-	 * @return 이메일 주소의 유효성 여부
-	 */
-	public static boolean checkEmailValidity(String email) {
-		if (email == null)
-			return false;
-		return EMAIL_PATTERN.matcher(email).matches();
+	public void deleteFoodReview(Integer foodId) {
+		personalFoodReviews.remove(foodId);
+	}
+
+	public void deletePersonalFavoriteFood(Integer foodId) {
+		personalFavoriteFoods.remove(foodId);
+	}
+
+	public void deletePersonalIngredient(Integer ingredientId) {
+		personalIngredients.remove(ingredientId);
 	}
 
 	@Override
@@ -94,17 +90,6 @@ public class UserAccount extends Account {
 				&& personalIngredients.equals(((UserAccount) another).personalIngredients)
 				&& personalFavoriteFoods.equals(((UserAccount) another).personalFavoriteFoods)
 				&& personalFoodReviews.equals(((UserAccount) another).personalFoodReviews);
-	}
-
-	/**
-	 * 개인 식재료 목록의 reference를 반환합니다.
-	 * 
-	 * 각 요소는 Integer 타입의 상품 ID로 이루어져 있습니다.
-	 * 
-	 * @return 개인 식재료 목록
-	 */
-	public List<Integer> getPersonalIngredients() {
-		return personalIngredients;
 	}
 
 	public String getEmail() {
@@ -132,4 +117,25 @@ public class UserAccount extends Account {
 	public List<Integer> getPersonalFoodReviews() {
 		return personalFoodReviews;
 	}
+
+	/**
+	 * 개인 식재료 목록의 reference를 반환합니다.
+	 * 
+	 * 각 요소는 Integer 타입의 상품 ID로 이루어져 있습니다.
+	 * 
+	 * @return 개인 식재료 목록
+	 */
+	public List<Integer> getPersonalIngredients() {
+		return personalIngredients;
+	}
+
+	public void printPersonalFavoriteFoods() {
+	}
+
+	public void printpersonalFoodReviews() {
+	}
+
+	public void printPersonalIngredients() {
+	}
+
 }
