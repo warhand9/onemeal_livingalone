8164a94d9cda1f4412185303d84d046f996179b3
Ahasil <lazyahasil+dev@gmail.com>
2018-05-31 00:14:42 +0900

    Added a list-filled customer to RecordReaderWriterTest

--- OneMealLivingAlone/src/test/java/org/onemeallivingalone/database/RecordReaderWriterTest.java
+++ OneMealLivingAlone/src/test/java/org/onemeallivingalone/database/RecordReaderWriterTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.onemeallivingalone.account.Account;
@@ -15,25 +14,32 @@ import org.onemeallivingalone.account.AdminAccount;
 import org.onemeallivingalone.account.CustomerAccount;
 
 public class RecordReaderWriterTest {
-	
+
 	private static String TEST_RECORDS_DIR = "test-data";
 
-	private Collection<Account> generateTestSetAccounts() {
-		List<Account> items = new ArrayList<>();
-		items.add(new AdminAccount("abcde", "abcde!12@$#"));
-		items.add(new CustomerAccount("abcde", "abcde!12@$#", "aaa@bbb.net"));
-		return items;
-	}
-	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		// Change logging level
 		org.apache.logging.log4j.core.config.Configurator.setLevel("", org.apache.logging.log4j.Level.WARN);
-		org.apache.logging.log4j.core.config.Configurator.setLevel("omla-data-driver", org.apache.logging.log4j.Level.TRACE);
+		org.apache.logging.log4j.core.config.Configurator.setLevel("omla-data-driver",
+				org.apache.logging.log4j.Level.TRACE);
 	}
 
-	@Before
-	public void setUp() throws Exception {
+	private Collection<Account> generateTestSetAccounts() {
+		List<Account> items = new ArrayList<>();
+		items.add(new AdminAccount("abcde", "abcde!12@$#"));
+		items.add(new CustomerAccount("fghsl", "abcde!12@$#", "aaa@bbb.net"));
+		{
+			CustomerAccount account1 = new CustomerAccount("wefwg", "abcde!12@$#", "bbb@ccc.net");
+			account1.addPeronsalFavoriteFood(10);
+			account1.addPeronsalFavoriteFood(20);
+			account1.addPersonalIngredient(100);
+			account1.addPersonalIngredient(200);
+			account1.addFoodReview(1000);
+			account1.addFoodReview(2000);
+			items.add(account1);
+		}
+		return items;
 	}
 
 	@Test
@@ -43,10 +49,10 @@ public class RecordReaderWriterTest {
 		FileManager fileManager = new FileManager();
 		RecordWriter recordWriter = new RecordWriter(fileManager);
 		recordWriter.writeJsonRecords(items, Account.class, TEST_RECORDS_DIR);
-		
+
 		RecordReader recordReader = new RecordReader(fileManager);
 		Collection<Object> itemsRead = recordReader.readJsonRecords(Account.class, TEST_RECORDS_DIR);
-		
+
 		assertEquals(items, itemsRead);
 	}
 
