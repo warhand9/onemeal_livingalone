cf20be4b9cd3973045c52fa9e8c2a894cc9de450
warhand <wise_9@naver.com>
2018-05-31 01:43:25 +0900

    IngredientSearchManagerUI 수정

--- OneMealLivingAlone/src/main/java/org/onemeallivingalone/item/ui/IngredientSearchManagerUI.java
+++ OneMealLivingAlone/src/main/java/org/onemeallivingalone/item/ui/IngredientSearchManagerUI.java
@@ -1,5 +1,8 @@
 package org.onemeallivingalone.item.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.onemeallivingalone.account.AdminAccount;
 import org.onemeallivingalone.account.CustomerAccount;
 import org.onemeallivingalone.center.CurrentUser;
@@ -8,7 +11,8 @@ import org.onemeallivingalone.item.Ingredient;
 import org.onemeallivingalone.item.IngredientList;
 
 public class IngredientSearchManagerUI extends ManagerUI{
-		
+	
+	CustomerAccount cusacnt;
 	String ingredientName;
 	Ingredient searchedIngredient=null;
 	int select;
@@ -24,7 +28,7 @@ public class IngredientSearchManagerUI extends ManagerUI{
 		System.out.println("검색할 식재료의 이름을 입력하십시오.");
 		ingredientName = scan.next();
 		
-		searchedIngredient = IngredientList.searchByName(ingredientName);
+		searchedIngredient = IngredientList.getInstance().searchByName(ingredientName);
 		System.out.println("검색결과>>");
 		if(searchedIngredient == null)
 		{
@@ -48,8 +52,8 @@ public class IngredientSearchManagerUI extends ManagerUI{
 				System.out.println("이용자 계정 로그인이 필요한 서비스 입니다.");
 				return;
 			}
-			CustomerAccount cusacnt = (CustomerAccount)CurrentUser.get();
-			cusacnt.addPersonalIngredient(searchedIngredient.getItemId());
+			cusacnt = (CustomerAccount)CurrentUser.get();
+			cusacnt.addPersonalIngredient(searchedIngredient.getIngredientId());
 			System.out.println(searchedIngredient.getName() +"이(가) 개인 식재료 리스트에 추가 되었습니다.");
 		}
 		
@@ -62,9 +66,36 @@ public class IngredientSearchManagerUI extends ManagerUI{
 		{
 			System.out.println(ingredient.getDescription());
 		}*/
+		
+		//
+		List<Ingredient> ingres = new ArrayList<Ingredient>(IngredientList.getInstance().getvalues());
+		int cur_page=1;
+		int listcnt=0;
+		int entire_page = (ingres.size()/5==0) ? ingres.size()/5 + 1 : ingres.size()/5+1+1;
+		int i;
+		
 		while(true)
 		{
 			// 5개 출력
+			//ingres = new ArrayList<Ingredient>(IngredientList.getInstance().getvalues());
+			//entire_page = (ingres.size()/5==0) ? ingres.size()/5 + 1 : ingres.size()/5+1+1;
+			listcnt = ingres.size();
+			if(listcnt==0)
+			{
+				System.out.println("시스템에 식재료 정보가 비었습니다.");
+				select=4;
+				return;
+			}
+			System.out.println("<식재료 목록>");
+			for(i=0; i<5; i++)
+			{
+				if(((cur_page-1)*5 + i)>=listcnt)
+					break;
+				
+				System.out.printf("%d. ",i+1);
+				System.out.println(ingres.get((cur_page-1)*5+i).getDescription());
+			}
+			//
 			
 			System.out.print("(1.이전페이지		2.다음페이지	3.검색하기		4.종료)입력 : ");
 			select = scan.nextInt();
@@ -76,8 +107,17 @@ public class IngredientSearchManagerUI extends ManagerUI{
 			switch (select)
 			{
 			case 1:
+				if(cur_page==1)
+					System.out.println("현재 페이지가 처음 페이지입니다.");
+				else
+					cur_page--;
 				break;
+				
 			case 2:
+				if(cur_page == entire_page)
+					System.out.println("현재 페이지가 마지막 페이지입니다.");
+				else
+					cur_page++;
 				break;
 			case 3:
 				return;
