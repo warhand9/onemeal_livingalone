32c29f5edf767aa266497efd2acd322d4a9d942d
warhand <wise_9@naver.com>
2018-05-31 01:44:49 +0900

    FoodSearchByIngredientsUI 수정

--- OneMealLivingAlone/src/main/java/org/onemeallivingalone/item/ui/FoodSearchByIngredientsUI.java
+++ OneMealLivingAlone/src/main/java/org/onemeallivingalone/item/ui/FoodSearchByIngredientsUI.java
@@ -1,23 +1,24 @@
 package org.onemeallivingalone.item.ui;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.onemeallivingalone.account.AdminAccount;
 import org.onemeallivingalone.account.CustomerAccount;
 import org.onemeallivingalone.center.CurrentUser;
 import org.onemeallivingalone.center.ui.ManagerUI;
-import org.onemeallivingalone.item.FoodList;
-import org.onemeallivingalone.item.IngredientList;
 import org.onemeallivingalone.item.*;
 
 public class FoodSearchByIngredientsUI extends ManagerUI{
 
+	CustomerAccount cusacnt;
 	ArrayList<Integer> ingredientsForSearch = new ArrayList<Integer>();
 	int ingredientID;
+	ArrayList<Food> searchedFood;
 	
 	@Override
 	public void interact() {
-		int select1, i;
+		int select1, i, cnt=0;
 		System.out.println("검색에 사용할 식재료들을 어디서 가지고 오시겠습니까(1.식재료검색기능을 통해	2.개인 식재료 리스트에서	3.종료)");
 		select1 = scan.nextInt();
 		while(true)
@@ -50,13 +51,23 @@ public class FoodSearchByIngredientsUI extends ManagerUI{
 		}
 		
 		System.out.println();
+		System.out.println("선택한 재료들>>");
 		for(i=0; i<ingredientsForSearch.size(); i++)
 		{	
-			System.out.print(IngredientList.get(ingredientsForSearch.get(i)).getName());
+			System.out.print(IngredientList.getInstance().get(ingredientsForSearch.get(i)).getName());
 			System.out.print(" ");
+			cnt++;
+			if(cnt%5==0)
+			{
+				System.out.println();
+				cnt=0;
+			}
 		}
 		System.out.print("\n위의 재료들로 검색한 결과>>\n");
+		searchedFood = (ArrayList<Food>)FoodList.getInstance().filteringByIngredients(ingredientsForSearch);
+		
 		// 결과 출력 // 복잡한 부분
+		showResultOfSearchByIngredients();
 		
 		new FoodViewUI().interact();
 		
@@ -67,9 +78,34 @@ public class FoodSearchByIngredientsUI extends ManagerUI{
 	{
 		int select;
 		
+		//
+		List<Ingredient> ingres = new ArrayList<Ingredient>(IngredientList.getInstance().getvalues());
+		int cur_page=1;
+		int listcnt=0;
+		int entire_page = (ingres.size()/5==0) ? ingres.size()/5 + 1 : ingres.size()/5+1+1;
+		int i;
+		
+		listcnt = ingres.size();
+		if(listcnt==0)
+		{
+			System.out.println("시스템에 식재료 정보가 비었습니다.");
+			select=4;
+			return;
+		}
+		
 		while(true)
 		{
 			// 5개씩 출력
+			System.out.println("<식재료 목록>");
+			for(i=0; i<5; i++)
+			{
+				if(((cur_page-1)*5 + i)>=listcnt)
+					break;
+				
+				System.out.printf("%d. ", i+1);
+				System.out.println(ingres.get((cur_page-1)*5+i).getDescription());
+			}
+			//
 			
 			System.out.print("(1.이전페이지		2.다음페이지	3.선택	4.선택종료) 입력 : ");
 			select = scan.nextInt();
@@ -81,13 +117,23 @@ public class FoodSearchByIngredientsUI extends ManagerUI{
 			switch(select)
 			{
 			case 1:
+				if(cur_page == 1)
+					System.out.println("현재 페이지가 처음 페이지입니다.");
+				else
+					cur_page--;
 				break;
+				
 			case 2:
+				if(cur_page == entire_page)
+					System.out.println("현재 페이지가 마지막 페이지입니다.");
+				else
+					cur_page++;
 				break;
+				
 			case 3:
 				System.out.print("식재료 번호를 입력하십시오 : ");
 				ingredientID = scan.nextInt();
-				if(IngredientList.get(ingredientID)==null)
+				if(IngredientList.getInstance().get(ingredientID)==null)
 					System.out.println("선택하신 식재료가 없습니다.");
 				else
 				{
@@ -136,13 +182,13 @@ public class FoodSearchByIngredientsUI extends ManagerUI{
 			System.out.println("이용자 계정 로그인이 필요한 서비스 입니다.");
 			return;
 		}
-		CustomerAccount cusacnt = (CustomerAccount)CurrentUser.get();
+		cusacnt = (CustomerAccount)CurrentUser.get();
 		
 		for(i=0; i<cusacnt.getPersonalIngredients().size(); i++)
 		{
 			int cnt=0;
-			Ingredient ingre = IngredientList.get(cusacnt.getPersonalIngredients().get(i));
-			System.out.print(ingre.getItemId() + "_" + ingre.getName() + " ");
+			Ingredient ingre = IngredientList.getInstance().get(cusacnt.getPersonalIngredients().get(i));
+			System.out.print(ingre.getIngredientId() + "_" + ingre.getName() + " ");
 			cnt++;
 			if(cnt%5 == 0)
 			{
@@ -169,6 +215,11 @@ public class FoodSearchByIngredientsUI extends ManagerUI{
 			}
 		}
 		System.out.println("검색에 이용할 식재료 선택이 완료되었습니다.");
+	}
+	
+	public void showResultOfSearchByIngredients()
+	{
+		
 	}
 
 }
