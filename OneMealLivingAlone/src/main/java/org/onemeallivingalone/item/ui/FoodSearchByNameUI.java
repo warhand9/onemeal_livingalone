063bc0ff482b29da71ee917fae9099c031dca8bc
warhand <wise_9@naver.com>
2018-05-31 01:44:26 +0900

    FoodSearchByNameUI 수정

--- OneMealLivingAlone/src/main/java/org/onemeallivingalone/item/ui/FoodSearchByNameUI.java
+++ OneMealLivingAlone/src/main/java/org/onemeallivingalone/item/ui/FoodSearchByNameUI.java
@@ -1,6 +1,7 @@
 package org.onemeallivingalone.item.ui;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.onemeallivingalone.center.ui.ManagerUI;
 import org.onemeallivingalone.item.Food;
@@ -10,23 +11,30 @@ public class FoodSearchByNameUI extends ManagerUI{
 
 	@Override
 	public void interact() {
-		int i;
+		int i=1;
 		String foodname;
-		ArrayList<Integer> filteringFoods = null; 
+		ArrayList<Food> filteringFoods = null; 
 		System.out.print("검색하고 하는 요리 이름을 입력하십시오. : ");
 		foodname = scan.next();
 		
-		if((filteringFoods=FoodList.filteringByName(foodname)) == null)
+		filteringFoods=(ArrayList<Food>)FoodList.getInstance().filteringByName(foodname);
+		
+		/*if((filteringFoods=FoodList.getInstance().filteringByName(foodname)) == null)
 		{
 			System.out.println("검색하신 요리가 없습니다.");
 			return;
+		}*/
+		for(Food f : filteringFoods)
+		{
+			System.out.printf("%d. ",i+1);
+			System.out.println(f.getSummaryDescription());
 		}
 		
-		for(i=0; i<filteringFoods.size(); i++)
+		/*for(i=0; i<filteringFoods.size(); i++)
 		{
 			Food food = FoodList.get(filteringFoods.get(i));
 			System.out.println(food.getSummaryDescription());
-		}
+		}*/
 		
 		new FoodViewUI().interact();
 		
