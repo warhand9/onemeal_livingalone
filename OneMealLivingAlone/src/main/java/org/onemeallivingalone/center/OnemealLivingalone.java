ee17a4eb7b1cec5dd5a9c1dc986b2b9c98546ac2
Ahasil <lazyahasil+dev@gmail.com>
2018-06-03 17:22:07 +0900

    Implemented file read & write in the "main()"

--- OneMealLivingAlone/src/main/java/org/onemeallivingalone/center/OnemealLivingalone.java
+++ OneMealLivingAlone/src/main/java/org/onemeallivingalone/center/OnemealLivingalone.java
@@ -1,23 +1,45 @@
 package org.onemeallivingalone.center;
 
 import org.onemeallivingalone.center.ui.MainMenuManagerUI;
+import org.onemeallivingalone.database.FileManager;
+import org.onemeallivingalone.database.RecordReader;
+import org.onemeallivingalone.database.RecordWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Main class.
+ *
+ * 메인 클래스입니다.
+ * 
+ * 로깅을 위해 디버그 환경에서는 아래 VM 인수를 씁니다.<br>
+ * -Dlog4j.configurationFile=log4j2-debug.xml
+ * <br>릴리스 환경에서는 아래 VM 인수를 씁니다.<br>
+ * -Dlog4j.configurationFile=log4j2.xml
+ */
 public class OnemealLivingalone {
-	
-	private static void readRecordsFromFiles()
-	{}
-	
-	private static void writeRecordsToFiles()
-	{}
-	
-	public static void main(String[] args)
-	{
-		readRecordsFromFiles();
-		
+
+	private static transient final Logger logger = LoggerFactory.getLogger("omla-center");
+
+	public static void main(String[] args) {
+		logger.info("OneMealLivingAlone just started");
+
+		// Import files
+		{
+			RecordReader recordReader = new RecordReader(new FileManager());
+			recordReader.readRecordsToLists();
+		}
+
 		MainMenuManagerUI mainMenuManager = new MainMenuManagerUI();
 		mainMenuManager.interact();
-		
-		writeRecordsToFiles();
+
+		// Export files
+		{
+			RecordWriter recordWriter = new RecordWriter(new FileManager());
+			recordWriter.writeRecordsFromLists();
+		}
+
+		logger.info("OneMealLivingAlone is closing now");
 	}
 
 }
