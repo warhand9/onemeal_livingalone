70ac9897b7a6236b9764a3bbdb2f820eda23f424
warhand <wise_9@naver.com>
2018-06-16 14:04:48 +0900

    회원가입 기능 수정

--- OneMealLivingAlone/src/main/java/org/onemeallivingalone/account/ui/GetMembershipManagerUI.java
+++ OneMealLivingAlone/src/main/java/org/onemeallivingalone/account/ui/GetMembershipManagerUI.java
@@ -38,6 +38,12 @@ public class GetMembershipManagerUI extends ManagerUI{
 			if(temp2.equals("0"))
 				return;
 			
+			if(Account.checkAccountIdValidity(accountID)==false)
+			{
+				System.out.println("ID 입력형식에 맞지 않습니다.");
+				System.out.println("계정 ID는 5글자 이상으로, 시작 문자는 알파벳이어야 하며, 나머지는 알파벳, 숫자, 밑줄('_')로 구성돼야 합니다.");
+				check = false;
+			}
 			if(AccountList.get(accountID) != null)
 			{
 				System.out.println("중복된 ID입니다.!!");
@@ -60,11 +66,13 @@ public class GetMembershipManagerUI extends ManagerUI{
 			if(!CustomerAccount.checkEmailValidity(email))
 			{
 				System.out.println("잘못된 email입니다.!!");
+				System.out.println("이메일 주소는 CustomerAccount.EMAIL_REGEX 에 지정된 정규 표현식에 맞아야 합니다.");
 				check = false;
 			}
 			if(!Account.checkPasswordValidity(temp1))
 			{
 				System.out.println("보안이 낮은 비밀번호입니다.!!");
+				System.out.println("비밀번호는 알파벳, 숫자, 특수문자 중 두 가지 이상으로 구성돼야 하며, 길이는 8자리 이상이어야 합니다.");
 				check = false;
 			}
 			if(!temp1.equals(temp2))
