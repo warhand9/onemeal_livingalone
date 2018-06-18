b62546dcde78437f6f87bcf2aa28dfde4c53aedc
Ahasil <lazyahasil+dev@gmail.com>
2018-06-18 20:38:23 +0900

    Fixed FoodIndexByIngredient.compareTo()

--- OneMealLivingAlone/src/main/java/org/onemeallivingalone/item/FoodIndexByIngredient.java
+++ OneMealLivingAlone/src/main/java/org/onemeallivingalone/item/FoodIndexByIngredient.java
@@ -1,27 +1,26 @@
 package org.onemeallivingalone.item;
 
-
 public class FoodIndexByIngredient implements Comparable<FoodIndexByIngredient> {
 
 	private final double sameness;
 	private final int extraCost;
 	private final Food food;
-	
+
 	public FoodIndexByIngredient(double sameness, int extraCost, Food food) {
 		this.sameness = sameness;
 		this.extraCost = extraCost;
 		this.food = food;
 	}
-	
+
 	@Override
 	public int compareTo(FoodIndexByIngredient other) {
-		int result = Double.compare(this.sameness, other.sameness);
+		int result = -Double.compare(this.sameness, other.sameness);
 		if (result == 0) {
 			result = Integer.compare(this.extraCost, other.extraCost);
 		}
 		return result;
 	}
-	
+
 	public int getExtraCost() {
 		return extraCost;
 	}
@@ -29,7 +28,7 @@ public class FoodIndexByIngredient implements Comparable<FoodIndexByIngredient> 
 	public Food getFood() {
 		return food;
 	}
-	
+
 	public double getSameness() {
 		return sameness;
 	}
